e6570795ddb61a2f63fa81f5f89b138943995004
SmallJimbo <dana.bichaoui@student.pxl.be>
2021-09-30 11:45:37 +0200

    kjhfd

--- introtest.py
+++ introtest.py
@@ -3,3 +3,6 @@ print("**** DND *********** Text Based Game *********** DND ****")
 print("****               made by Dana & Axel               ****")
 print("****                                                 ****")
 print("*********************************************************")
+
+def start():
+    print(Its 11pm, you are getting ready "for" your night shit)
