0fb9656da8290069636a715b328781381a91798b
Subaraki <robbeenaxel@hotmail.com>
2021-09-30 12:25:06 +0200

    fix start
    start wasnt called

--- introtest.py
+++ introtest.py
@@ -1,8 +1,10 @@
+def start():
+    print("It's 2am")
+
 print("*********************************************************")
 print("**** DND *********** Text Based Game *********** DND ****")
 print("****               made by Dana & Axel               ****")
 print("****                                                 ****")
 print("*********************************************************")
 
-def start():
-    print("It's 2am")
+start()
